4003d9987b7cb6fd85e35d02b0738cee9d9a86b9
Stepan <snikoghosyan@servicetitan.com>
2022-06-10 11:37:27 +0400

    remove js

--- hopar/models/language.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Languages = void 0;
-var Languages;
-(function (Languages) {
-    Languages["EN"] = "en";
-    Languages["FR"] = "fr";
-})(Languages = exports.Languages || (exports.Languages = {}));
--- hopar/models/translations.model.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var language_model_1 = require("./language.model");
